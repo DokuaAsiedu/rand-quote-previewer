b7b707e9b29e8b852fe2d6085f4e840f2f1c047d
DokuaAsiedu <asiedudokua@gmail.com>
2024-04-01 16:03:15 +0000

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { quoteData } from "./config";
 import { randomHex } from "./utils";
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa6";
@@ -16,12 +16,13 @@ function App() {
 		setCol(randomHex());
 	};
 
+	useEffect(() => {
+		document.body.style.backgroundColor = `#${col}`;
+	}, [col]);
+
 	return (
-		<div
-			className="h-full min-h-screen py-12 flex flex-col items-center justify-center relative transition-colors ease-in-out duration-1000 text-base md:text-lg lg:text-2xl xl:text-4xl"
-			style={{ backgroundColor: `#${col}` }}
-		>
-			<div className="w-4/5 sm:w-3/5 2xl:w-2/5 flex flex-col justify-between align-center gap-4 bg-white rounded-md p-8 transition-colors ease-in-out duration-1000">
+		<div className="h-full min-h-screen min-w-[280px] py-12 flex flex-col items-center justify-center relative transition-colors ease-in-out duration-1000 text-xl">
+			<div className="w-11/12 sm:w-3/5 flex flex-col justify-between align-center gap-4 bg-white rounded-md p-8 transition-colors ease-in-out duration-1000">
 				<p
 					className="transition-colors ease-in-out duration-1000"
 					style={{ color: `#${col}` }}
@@ -31,7 +32,7 @@ function App() {
 					<FaQuoteRight className="inline-block align-top" />
 				</p>
 				<p
-					className="self-end transition-colors ease-in-out duration-1000"
+					className="self-end transition-colors ease-in-out duration-1000 text-sm"
 					style={{ color: `#${col}` }}
 				>
 					- {quote.author}
@@ -39,17 +40,19 @@ function App() {
 				<div className="w-full flex flex-row items-stretch justify-between">
 					<div className="flex flex-start items-center justify-start gap-4">
 						<IconContext.Provider value={{ style: { fill: "white" } }}>
-							<button
-								className="flex flex-col items-center justify-center rounded-lg h-full aspect-square p-2 xl:p-4 transition-colors ease-in-out duration-1000"
-								style={{ backgroundColor: `#${col}` }}
-							>
-								<FaGithub className="h-full" />
-							</button>
+							<a href="https://github.com/DokuaAsiedu">
+								<button
+									className="flex flex-col items-center justify-center rounded-lg h-full aspect-square p-2 transition-colors ease-in-out duration-1000"
+									style={{ backgroundColor: `#${col}` }}
+								>
+									<FaGithub className="h-full" />
+								</button>
+							</a>
 						</IconContext.Provider>
 					</div>
 					<button
 						onClick={changeQuote}
-						className="py-2 px-4 xl:p-6 rounded-lg p-2 text-white transition-colors ease-in-out duration-1000"
+						className="py-2 px-4 rounded-lg p-2 text-white transition-colors ease-in-out duration-1000"
 						style={{ backgroundColor: `#${col}` }}
 					>
 						New Quote
